1c6a5f8b5b691d1298d1ca20ed7b6d5619db18d5
Lloyd Stevens <stevens.lloyd@gmail.com>
2023-06-24 22:42:25 +0100

    commit

--- app/compsci/page.tsx
+++ app/compsci/page.tsx
@@ -2,7 +2,7 @@
 import useLLM from "usellm";
 import { useState } from "react";
 
-export default function Home() {
+export default function Compsci() {
   const llm = useLLM();
   const [topic, setTopic] = useState("");
   const [result, setResult] = useState("");
--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,6 @@ export default function Home() {
  
   return (
     <div className="min-h-screen mx-auto my-8 max-w-4xl">
-      <h1 className="text-center mb-4 text-2xl">Do Now Generator</h1>
       <Compsci />
     </div>
   );
